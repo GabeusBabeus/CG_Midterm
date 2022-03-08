1f1b608cf48501f46dddfb9fbd8b67b77399656d
Robbie Strauch <robbie.strauch@gmail.com>
2022-03-08 10:02:54 -0500

    Movement Behaviour

---
+++ OTTER/projects/Week6-Tutorial/src/Gameplay/Components/MovementBehaviour.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "IComponent.h"
+#include "Gameplay/Physics/RigidBody.h"
+
+/// <summary>
+/// Simple WASD movement
+/// </summary>
+class MovementBehaviour : public Gameplay::IComponent {
+public:
+	typedef std::shared_ptr<MovementBehaviour> Sptr;
+
+	std::weak_ptr<Gameplay::IComponent> Panel;
+
+	MovementBehaviour();
+	virtual ~MovementBehaviour();
+
+	virtual void Awake() override;
+	virtual void Update(float deltaTime) override;
+
+public:
+	virtual void RenderImGui() override;
+	MAKE_TYPENAME(MovementBehaviour);
+	virtual nlohmann::json ToJson() const override;
+	static MovementBehaviour::Sptr FromJson(const nlohmann::json& blob);
+
+protected:
+	float _impulse;
+
+	bool _isPressed = false;
+	Gameplay::Physics::RigidBody::Sptr _body;
+};
